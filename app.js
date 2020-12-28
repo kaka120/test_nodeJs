a06f6654a670fc8b0e5f6b2a1e8df34d3c6c8da1
kaka120 <knowledgebase006@gmail.com>
2020-12-28 17:26:11 +0530

    33

--- app.js
+++ app.js
@@ -26,16 +26,16 @@ const port = process.env.PORT || '5000';
 
 
 console.log(env_custom_variable.parsed.MONGO_URI)
-
+/*
 mongoose.connect(env_custom_variable.parsed.MONGO_URI)
   .then((result) => app.listen(port,function(){
     console.log(`Server started on Port ${port}`);
   }))
   .catch((err) => console.log(err));
 app.use('/api', parent_router); 
-
-// app.listen(port,function(){
-//     console.log("Testing Server is listning at port 5000")
-// })
+*/
+ app.listen(port,function(){
+     console.log("Testing Server is listning at port 5000")
+ })
 
 app.get('/test', (req,res) => res.send(`<h1>Deployed on Heroku</h1>`))
