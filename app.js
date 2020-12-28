9e59b1f1cad7edb0116cbd2a32fa3badb4f6e9a3
kaka120 <knowledgebase006@gmail.com>
2020-12-28 18:23:36 +0530

    aaa

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(upload.array()); 
 app.use(express.static('public'));
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '5000' ;
 
 
 
