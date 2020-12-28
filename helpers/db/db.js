c7062bd405e91fb6d49f173c656f2b008c299e1d
Raja Dastidar <rajadastidarking@gmail.com>
2020-12-28 20:39:45 +0530

    error solver connection related
    error solver connection related

--- helpers/db/db.js
+++ helpers/db/db.js
@@ -3,8 +3,8 @@ const config = require('../../config/config');
 const mongoose = require('mongoose');
 console.log(config)
 
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+// mongoose.Promise = global.Promise;
 
 // const bcrypt = require('bcryptjs');
 // const mongoose = require('mongoose');
