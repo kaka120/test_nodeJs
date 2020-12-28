8d4ba57bb0593ba0a7348deae5a49f9541643241
kaka120 <knowledgebase006@gmail.com>
2020-12-28 14:11:17 +0530

    ab

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(upload.array());
 app.use(express.static('public'));
 const port = process.env.PORT || '5000';
 
-const dbURI = 'mongodb+srv://admintest:admin123test@cluster0.y02b6.mongodb.net/test12?retryWrites=true&w=majority'
+const dbURI = ''
 mongoose.connect(dbURI)
   .then((result) => app.listen(port,function(){
       console.log("Testing Server is listning at port 5000")
