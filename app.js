f97d07ab48fcc83e50ae04d4fa7619059bb98380
kaka120 <knowledgebase006@gmail.com>
2020-12-28 13:27:04 +0530

    123

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var multer = require('multer');
-
-const mongoose = require('mongoose');
-
+ /*
+ const mongoose = require('mongoose');
  mongoose.set('useNewUrlParser', true);
  mongoose.set('useFindAndModify', false);
  mongoose.set('useCreateIndex', true);
  mongoose.set('useUnifiedTopology', true);
-
+*/
 var upload = multer();
 const app= express() 
 var cors = require('cors')
 // Add headers
 app.use(cors())
+/*
 const parent_router = require('./routes/parent_router');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(upload.array()); 
 app.use(express.static('public'));
-
+*/
 
 //const dbURI = 'mongodb://localhost/test_db';
 //const dbURI = 'mongodb+srv://admintest:admin123test@cluster0.y02b6.mongodb.net/test12'
